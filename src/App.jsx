814d9d97af86c0a82161be7be077695540071225
Lara286 <akekitokito2009@gmail.com>
2025-09-27 18:05:41 -0300

    Home-Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import Contacto from "./componentes/Contacto.jsx"
+import Navbar from "./componentes/Nav.jsx"
 function App() {
 
   return (
     <>
-      
+      <Navbar/> 
+      <Contacto/>
     </>
   )
 }
---
+++ src/componentes/Nav.jsx
@@ -0,0 +1,41 @@
+export default function Navbar(){
+    return(
+        <>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">¿Quienes somos?</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active " aria-current="page" href="#"><img className="logos" src="/img/logo_carrito.jpg" alt="" /></a>
+                  </li>
+                  <li class="nav-item d-flex ms-auto">
+                    <a class="nav-link" href="#"><img className="logos" src="/img/logo_whatsapp.png" alt="" /></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><img className="logos" src="/img/logo_instagram.png" alt="" /></a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Collares</a></li>
+                      <li><a class="dropdown-item" href="#">Pulseras</a></li>
+                      <li><a class="dropdown-item" href="#">Llaveros</a></li>
+                      <li><a class="dropdown-item" href="#">Señaladores</a></li>
+                      <li><a class="dropdown-item" href="#">Aros</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex mx-auto" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                  <button class="btn btn-outline-success" type="submit">🔍</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </>
+    )
+}
