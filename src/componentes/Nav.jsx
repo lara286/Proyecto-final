e0ec62a943d78b964e209652b59f8831f5e21d64
lara286 <akekitokito2009@gmail.com>
2025-10-12 19:19:42 -0300

    primer-entregable

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -4,12 +4,15 @@ export default function Navbar(){
         <>
           <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
             <div class="container-fluid">
-              <Link to= "/quienes_somos" className="navbar-brand" >¿Quienes somos?</Link>
+              <Link to= "/" className="navbar-brand" ><img className="logos" src="/img/home.jpg" alt="" /></Link>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <Link to="/quienes_somos" class="nav-link active " aria-current="page" >¿Quienes somos?</Link>
+                  </li>
                   <li class="nav-item">
                     <Link to="/carrito" class="nav-link active " aria-current="page" ><img className="logos" src="/img/logo_carrito.jpg" alt="" /></Link>
                   </li>
@@ -25,7 +28,7 @@ export default function Navbar(){
                       <li><a class="dropdown-item" href="#">Collares</a></li>
                       <li><a class="dropdown-item" href="#">Pulseras</a></li>
                       <li><a class="dropdown-item" href="#">Llaveros</a></li>
-                      <li><a class="dropdown-item" href="#">Señaladores</a></li>
+                      <li><a class="dropdown-item" href="#">Broches y Vinchas</a></li>
                       <li><a class="dropdown-item" href="#">Aros</a></li>
                     </ul>
                   </li>
--- src/componentes/Quienes_somos.jsx
+++ src/componentes/Quienes_somos.jsx
@@ -1,10 +1,38 @@
 export default function Informacion (){
 return (
     <>
-    <div className="padding" >
-        
-    </div>
-    
+        <div className="padding" >
+            <h4 className="titulo-informacion">Este es un microEmprendimiento de accesorios donde su produccion es totalmente artesanal</h4>
+            <div className="col-12 col-lg-11">
+                <div className="grilla-informacion">
+                    <div className="element-grilla" >
+                        <h4 className="titulo-element">Historia</h4>
+                        <p>
+                        Hace ya dos años que nos dedicamos a proveerles a nuestros clientes productos personalizados de la mejor 
+                        calidad posible. Este emprendimiento surgio gracias a dos jovenes que no conseguian los accesorios que ellas
+                        querian, por lo que decidieron crearlos con sus propias manos y aprendieron como hacerlos para luego compartir 
+                        esa posibilidad de tener el detalle perfecto para cualquier persona y ocasión.
+                        </p>
+                    </div>
+                    <div className="element-grilla">
+                        <h4 className="titulo-element">Mision</h4>
+                        <p>
+                        Diseñar y elaborar accsesorios artesanales únicos que reflejen la identidad, creatividad y estilo personal
+                        de cada cliente, combinando materiales de calidad con un trabajo hecho a mano con dedicación.
+                        </p>
+                    </div>
+                    <div className="element-grilla">
+                        <h4 className="titulo-element">Vision</h4>
+                        <p>
+                        Ser un emprendimiento reconocido por la originalidad y calidad de nuestros accesorios artesanales,
+                        expandiendonos en el mercado local y digital hasta lograr ser una marca referente en accesorios, 
+                        destacada por su diseño auténtico.
+                        </p>
+                    </div>
+                    <h6 className="titulo-informacion">Nuestro lema es: "Despliega tus alas, despliega tu belleza"</h6>
+                </div>
+            </div> 
+        </div>
     </>
 )
 }
